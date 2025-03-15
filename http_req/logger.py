7d1216d4b93ce7384822771b9ae1b78c5844142b
fabian <fbarulli@gmail.com>
2025-03-15 14:52:58 +0000

    logging

--- http_req/logger.py
+++ http_req/logger.py
@@ -7,19 +7,20 @@ import os
 
 
 log_dir = "logs"
-os.mkdir(log_dir, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
 
 class TextLogger:
 
-    def __init__(self, filename: str = "app.log", log_dir: str = "logs") -> None:
+    def __init__(self, filename: str = "app.log", log_dir: str = "logs", level int = logging.DEBUG) -> None:
         
         os.makedirs(log_dir, exist_ok=True) #create log_dir
+        self.logger.setLevel(level)
         self.logger: logging.Logger = logging.getLogger("FastAPI") #create logger
         self.logger.setLevel(logging.DEBUG) 
         
         log_path : str = os.path.join(log_dir, filename)
         handler = logging.FileHandler(log_path)
-        formatter = logging.Formatter(fmt = "%(asctime)s - %(levelname)s - %(messeage)s")
+        formatter = logging.Formatter(fmt = "%(asctime)s - %(levelname)s - %(message)s  - %(endpoint)s  - %(method)s ")
         
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
--- http_req/main.py
+++ http_req/main.py
@@ -1,4 +1,4 @@
-import os
+from logger import logger
 from fastapi import FastAPI
 import traceback
 
@@ -7,4 +7,8 @@ api = FastAPI()
 
 @api.get('/')
 def get_index():
+    logger.logger.info("get_index() has started")
+    try:
+    except Exception as e:    
+    
     return {'data': 'hello world'}
