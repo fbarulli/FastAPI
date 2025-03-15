4f33013c8ede144506e7753757bf6c68a2000d11
fabian <fbarulli@gmail.com>
2025-03-15 21:40:15 +0000

    lol

--- passing_data/app.py
+++ passing_data/app.py
@@ -29,8 +29,24 @@ def get_user_by_name(name: str) -> Optional[Union[User, str]]:
     result = [user for user in users_db if user.name == name]
     return result[0] if result else ""
 
-@app.get("/users/{user_id}/subscription")
+@app.get("/user/{user_id}/subscription")
 @logger.log_function
 def get_sub_by_name(user_id: int) -> Optional[Union[User, str]]:
     result = [user for user in users_db if user.user_id == user_id]
     return result[0].subscription if result else ""
+
+
+#  Request string
+from typing import Optional
+@app.get('/addition')
+@logger.log_function
+def get_addition(a: int, b: Optional[int]=None):
+    if b:
+        result = a + b
+    else:
+        result = a + 1
+    return {
+        'addition_result': result
+    }
+    
+#  Headers
