a9692e9bda35336e09037f18d80f6703ad0618f3
fabian <fbarulli@gmail.com>
2025-03-15 12:59:42 +0000

    lol

--- http_req/logger.py
+++ http_req/logger.py
@@ -1,3 +1,6 @@
+# logger.py
+
+
 import logging
 import os
 from typing import Optional
@@ -11,11 +14,15 @@ class TextLogger:
     def __init__(self, filename: str = "app.log", log_dir: str = "logs") -> None:
         
         os.makedirs(log_dir, exist_ok=True) #create log_dir
-        
-        self.logger: logging.Logger = logging.getlogger("FastAPI") #create logger
-        self.logger.setLevel(logging.INFO)
+        self.logger: logging.Logger = logging.getLogger("FastAPI") #create logger
+        self.logger.setLevel(logging.DEBUG) 
         
         log_path : str = os.path.join(log_dir, filename)
-        formatter : logging.Formatter = logging.FileHandler(log_)
+        handler = logging.FileHandler(log_path)
+        formatter = logging.Formatter(fmt = "%(asctime)s - %(levelname)s - %(messeage)s")
+        
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
         
         
+logger : TextLogger = TextLogger()
