c40d87fee396e6a8a41d7c8aee877db716f132c3
fabian <fbarulli@gmail.com>
2025-03-15 20:18:34 +0000

    2nd part

--- passing_data/app.py
+++ passing_data/app.py
@@ -1,6 +1,8 @@
 from http_req.logger import logger
 from fastapi import FastAPI
-from typing import Optional, TypedDict
+from pydantic import BaseModel
+from typing import Optional
+
 
 app = FastAPI()
 
@@ -23,21 +25,16 @@ users_db = [
     }
 ]
 
+class User(BaseModel):
+    user_id: int
+    name: str
+    subscription: str
+
+users_db = [User(**user) for user in users_db]
 
 
-
-
-
-class User(TypeDict):
-    user_id :int
-    name : str
-    subscription :str
-
-
-
-
-@api.get('/user/{user_id:int}/')
+@app.get('/user/{user_id:int}/')
 @logger.log_function
 def get_user_by_id(user_id: int) -> Optional[User]:
     result = [user for user in users_db if user['user_id'] == user_id]
-    return result[0] if result else None  
+    return result[0] if result else None
