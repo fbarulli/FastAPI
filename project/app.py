052805727a76cc7b269e526fb07e612c2e28dc71
fabian <fbarulli@gmail.com>
2025-03-17 13:20:28 +0000

    lol

--- project/app.py
+++ project/app.py
@@ -1,4 +1,3 @@
-# app.py
 import sys 
 import os 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
@@ -6,48 +5,42 @@ print("sys.path:", sys.path)
 print("Current dir:", os.getcwd())   
 
 from fastapi import FastAPI, HTTPException, Request
-from project.logger.logger import TextLogger as logger
 from project.data.data import Question, questions
-from typing import Dict, Any, Callable, Coroutine  
+from typing import Dict, Any, List
 
 app = FastAPI()  
 
-def handle_errors(func: Callable[..., Coroutine[Any, Any, Dict[str, Any]]]) -> Callable[..., Coroutine[Any, Any, Dict[str, Any]]]:     
-    """     
-    A wrapper function to handle errors in FastAPI endpoints.     
-    """     
-    async def wrapper(*args, **kwargs) -> Dict[str, Any]:         
-        try:             
-            return await func(*args, **kwargs)         
-        except Exception as e:             
-            error_msg = f"Failed in {func.__name__}: {str(e)}"             
-            raise HTTPException(status_code=500, detail=error_msg)          
-    return wrapper   
-
-#!curl -X GET "http://127.0.0.1:8000/questions" 
+#!curl -X GET "http://127.0.0.1:8000/questions"
 @app.get("/questions") 
-@handle_errors 
-async def get_all_questions(request: Request) -> Dict[str, Any]:     
+async def get_all_questions():     
     '''     
     returns : dict with all questions     
-    '''     
-    return {"questions": questions} 
+    '''
+    try:
+        return {"questions": [q.model_dump() for q in questions]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to get questions: {str(e)}")
 
-#!curl -X GET "http://127.0.0.1:8000/questions/use" 
 @app.get("/questions/use") 
-@handle_errors 
-async def get_all_uses(request: Request) -> Dict[str, Any]:     
+async def get_all_uses():     
     '''     
     returns : dict with all uses     
-    '''     
-    unique_uses = set(q.use for q in questions)     
-    return {"uses": unique_uses}       
+    '''
+    try:
+        unique_uses = list(set(q.use for q in questions))
+        return {"uses": unique_uses}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to get uses: {str(e)}")
 
+
+# !curl -X GET "http://127.0.0.1:8000/questions/use"
 @app.get('/questions/{use}') 
-@handle_errors 
-async def get_questions_by_use(use: str, request: Request) -> Dict[str, Any]:     
+async def get_questions_by_use(use: str):     
     '''     
     returns : dict filtered by use     
-    '''     
-    questions_by_use = [q for q in questions if q.use == use]     
-    return {"questions_by_use": questions_by_use}
+    '''
+    try:
+        questions_by_use = [q.model_dump() for q in questions if q.use == use]
+        return {"questions_by_use": questions_by_use}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to get questions by use: {str(e)}")
